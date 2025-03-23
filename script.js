05bb2b2f133807c04d0b6ba79f1ae485d2d8e9fa
theMungai <mungaithesimon@gmail.com>
2025-03-23 17:24:13 +0300

    Display the content from JSON to the DOM

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
 function fetchMovies(){
     fetch("http://localhost:3000/films")
     .then((response) => response.json())
-    .then((movies) => displayMovies(movies))
+    .then((movies) => movies.forEach((movie) => displayMovies(movie)))
+    .catch((error) => console.error(error))
 }
 fetchMovies()
 
-function displayMovies(movies){
+function displayMovies(movie){
     
-        document.querySelector("#films").innerHTML = movies
-        .map((movie) => `<li>${movie.title}</li>`)
-    
+        // Movie Titles
+        let movieTitles = document.createElement("div")
+        movieTitles.innerHTML = `<p>${movie.title}  <button>Delete</button> </p>`
+        document.querySelector("#films").appendChild(movieTitles)
+
+        movieTitles.addEventListener("click", () => {
+            loadMovies(movie)
+        })
+
+        // Movie Posters
+        let moviePosters = document.createElement("div")
+        moviePosters.innerHTML = `<img src = "${movie.poster}">`
+        document.querySelector(".movie-posters").appendChild(moviePosters)
+
+        // Create movie card
+        let card = document.createElement("div")
+
+        card.innerHTML = `
+            <div class = "card-header">
+                <h2>${movie.title}</h2>
+                <p>${movie.runtime} minutes</p>
+            </div>
+
+            <div class = "movie-descriptions">
+                <p>${movie.description}</p>
+
+                <div class = "time-and-tickets">
+                    <p>${movie.showtime}</p>
+                    <p><span>${movie.capacity - movie.tickets_sold}</span> remaining tickets</p>
+                </div>
+                <button class = "buy-ticket-button">Buy Ticket</button>
+            </div>
+        `;
+
+        document.querySelector(".movie-details").appendChild(card)
+
+        
+}
+
+
+function loadMovies(movie){
+
 }
